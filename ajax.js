115f8d401e86323d43d164811d7c205de57efe90
julia0903 <45658133+julia0903@users.noreply.github.com>
2018-12-06 21:12:08 +0900

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
     $("button").click(function(){
         $.get("ajax.json", function(data, status){
-$("#demo").html("<b>Iam: </b>" + data.Iam + "<br><b>나는: </b>" + data.나는);
+$("#demo").html("<b>이름 </b>" + data.name + "<br><b>고향 </b>" + data.home);
         });
     });
 });
