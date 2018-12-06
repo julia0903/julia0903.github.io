92cee29c30390cc9db8d26c5f02361e807925596
julia0903 <45658133+julia0903@users.noreply.github.com>
2018-12-06 20:39:24 +0900

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $.get("ajax_info.txt", function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
